3f0f3b822bc581645ad5dfde9a61d06d782cb030
Haopeng <hf0242a@student.american.edu>
2019-10-08 11:34:06 -0400

    123

---
+++ exercises/game01/Game01/Assets/Scripts/PlanePilot.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlanePilot : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float speed = 40.0f;
+    public Text lostText;
+    void Start()
+    {
+        lostText.text = "";
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 moveCamTo = transform.position - transform.forward * 10.0f + Vector3.up * 5.0f;
+        float bias = 0.96f;
+        Camera.main.transform.position = Camera.main.transform.position * bias + moveCamTo * (1.0f - bias);
+        Camera.main.transform.LookAt(transform.position + transform.forward*30.0f);
+        transform.position += transform.forward * Time.deltaTime * speed;
+        speed -= transform.forward.y * Time.deltaTime * 50.0f;
+
+        transform.Rotate(Input.GetAxis("Vertical"), 0.0f, -Input.GetAxis("Horizontal"));
+
+        float terrainHeightWhereWeAre = Terrain.activeTerrain.SampleHeight(transform.position);
+
+        if(terrainHeightWhereWeAre > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, terrainHeightWhereWeAre, transform.position.z);
+        }
+
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        lostText.text = "You Are Died!";
+    }
+}

c9c7b9bd8847a5e364f52b3ba757493350d857e0
Haopeng <hf0242a@student.american.edu>
2019-10-09 16:29:22 -0400

    123

---
+++ exercises/game02/GameOfLife/Assets/CellScript.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellScript : MonoBehaviour
+{
+	public bool alive = false;
+	public bool nextAlive;
+	bool prevAlive;
+	public int x = -1;
+	public int y = -1;
+    public Texture[] textures;
+    public Renderer renderer;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		prevAlive = alive;
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (prevAlive != alive) {
+			updateColor();
+		}
+
+		prevAlive = alive;
+	}
+
+	public void updateColor()
+	{
+		if (renderer == null) {
+			renderer = gameObject.GetComponent<Renderer>();
+		}
+
+		if (this.alive) {
+			renderer.material.color = Color.magenta;
+            gameObject.transform.localScale = new Vector3(3.3f, 7.3f, 3.3f);
+        } else {
+			renderer.material.color = Color.yellow;
+            gameObject.transform.localScale = new Vector3(3.3f, 3.3f, 3.3f);
+        }
+	}
+
+
+	private void OnMouseDown()
+	{
+		alive = !alive;
+	}
+}
